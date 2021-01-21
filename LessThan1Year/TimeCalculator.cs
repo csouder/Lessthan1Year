2eaaa420a5b4526d4b80bcc3c53c6cdccc2f55a6
csouder <77806064+csouder@users.noreply.github.com>
2021-01-21 15:36:01 -0500

    Add files via upload

---
+++ LessThan1Year/TimeCalculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessThan1Year
+{
+    public class TimeCalculator
+    {
+        /// <summary>
+        /// Returns true if the end date is less than one year after the start
+        /// date. The time component will be considered when determining the
+        /// result.
+        /// </summary>
+        /// <param name="startDate">The start date and time.</param>
+        /// <param name="endDate">The end date and time.</param>
+        /// <returns>True if the range is less than one year, false
+        /// otherwise.</returns>
+        public static bool IsDurationLessThanOneYear(DateTime startDate, DateTime endDate)
+        {
+            if (DateTime.Compare(startDate, endDate) > 0)
+                throw new ArgumentException();
+ 
+            if (endDate.Year > startDate.Year + 1)
+                return false;
+            else
+            {
+                if (endDate.Year > startDate.Year)
+                {
+                    if (endDate.Month > startDate.Month)
+                        return false;
+                    else
+                    {
+                        if (endDate.Day >= startDate.Day)
+                            return false;
+                    }
+                }
+            }
+            
+            return true;
+        }
+    }
+}
---
+++ Lessthan1yearTest/Lessthan1YearTest.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Lessthan1yearTest
+{
+    [TestClass]
+    public class Lessthan1YearTest
+    {
+        [TestMethod]
+        public void SameYearTest()
+        {
+            DateTime start = new DateTime(2020, 1, 20);
+            DateTime end = new DateTime(2020, 6, 4);
+            bool timeframe = LessThan1Year.TimeCalculator.IsDurationLessThanOneYear(start, end);
+            Assert.IsTrue(timeframe);
+        }
+
+        [TestMethod]
+        public void NextYearTest()
+        {
+            DateTime start = new DateTime(2020, 1, 20);
+            DateTime end = new DateTime(2021, 1, 4);
+            bool timeframe = LessThan1Year.TimeCalculator.IsDurationLessThanOneYear(start, end);
+            Assert.IsTrue(timeframe);
+        }
+
+        [TestMethod]
+        public void ExactlyOneYear()
+        {
+            DateTime start = new DateTime(2020, 1, 20);
+            DateTime end = new DateTime(2021, 1, 20);
+            bool timeframe = LessThan1Year.TimeCalculator.IsDurationLessThanOneYear(start, end);
+            Assert.IsFalse(timeframe);
+        }
+
+        [TestMethod]
+        public void MoreThanOneYear()
+        {
+            DateTime start = new DateTime(2020, 1, 20);
+            DateTime end = new DateTime(2021, 1, 21);
+            bool timeframe = LessThan1Year.TimeCalculator.IsDurationLessThanOneYear(start, end);
+            Assert.IsFalse(timeframe);
+        }
+
+        [TestMethod]
+        [TestCategory("Invalid")]
+        [ExpectedException(typeof(ArgumentException))]
+        public void EndBeforeStart()
+        {
+            DateTime start = new DateTime(2021, 1, 20);
+            DateTime end = new DateTime(2020, 1, 21);
+            bool timeframe = LessThan1Year.TimeCalculator.IsDurationLessThanOneYear(start, end);
+            Assert.Fail("Exception not thrown");
+        }
+    }
+}
